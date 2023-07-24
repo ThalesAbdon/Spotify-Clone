dbfca76238bd5af47d243f30bd9812300154f016
ThalesAbdon <thales.sousufpi@gmail.com>
2023-07-24 14:29:52 -0300

    feat: Providers for auth and supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import Sidebar from '@/components/sidebar'
+import Sidebar from '@/components/Sidebar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupaBaseProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -18,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-        {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <Sidebar>
+          {children}
+          </Sidebar>
+        </SupabaseProvider>
         </body>
     </html>
   )
